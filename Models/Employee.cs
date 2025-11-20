cea3f0c4098f47932881a4e5a720c9cc18f6a6a3
Rakesh7586-art <rakesh.myth@gmail.com>
2025-11-20 21:42:16 +0530

    Create Employee.cs
    created Employee model class

---
+++ Models/Employee.cs
@@ -0,0 +1,12 @@
+namespace BigCommpanyAnalyzer.Models;
+
+public class Employee
+{
+  public int Id { get; set;}
+  public string FirstName{ get; set;}="";
+  public string LastName{ get; set;}="";
+  public decimal Salary { get; set;}
+  public int? ManagerId { get; set;}
+
+  public List<Employee> Subordinates { get; set;} = new();
+}
